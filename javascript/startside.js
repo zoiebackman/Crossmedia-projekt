852a1ae55590e311cb09ae092d3f90906b10bc6f
zoiebackman <zoiebackman@icloud.com>
2026-04-21 14:43:59 +0200

    update homepage

--- javascript/startside.js
+++ javascript/startside.js
@@ -8,5 +8,31 @@ let main = document.querySelector("main");
 
 function homePage() {
   main.innerHTML = ``;
-  let 
+  main.style.display = "flex";
+  main.style.flexDirection = "column";
+  main.style.alignItems = "center";
+
+  let whisperBox = document.createElement("div");
+  whisperBox.id = "whisperBox";
+  let whisperTitle = document.createElement("h1");
+  whisperTitle.textContent = "Senaste viskningar...";
+  whisperTitle.id = "whisperTitle";
+  whisperBox.append(whisperTitle);
+
+  let div = document.createElement("div");
+  div.id = "gossipBox";
+
+  let allTipsBox = document.createElement("div");
+  allTipsBox.id = "allTipsBox";
+  let allTips = document.createElement("p");
+  allTips.textContent = "alla gossips";
+  allTips.id = "allTips";
+  allTipsBox.append(allTips);
+
+  main.append(whisperBox);
+  main.append(div);
+
+  main.append(allTipsBox);
 }
+
+homePage();
