e47692d3e8cbfe3c9531c37c9520f9a086880199
zoiebackman <zoiebackman@icloud.com>
2026-04-21 15:25:35 +0200

    update homepage function

--- javascript/startside.js
+++ javascript/startside.js
@@ -8,31 +8,30 @@ let main = document.querySelector("main");
 
 function homePage() {
   main.innerHTML = ``;
+  main.innerHTML = `
+    <div id="whisperBox">
+        <p id="whisperTitle">Senaste viskningar...</p>
+    </div>
+    <div id="gossipBox"></div>
+    <div id="allTipsBox">
+        <p class="allTipsAllPictures">alla gossips</p>
+    </div>
+    <div id="selectedPicturesBox">
+        <p id="selectedPicturesText">Utvalda ögonblick</p>
+    </div>
+    <div id="picturesBox">
+        <div id="picture1"></div>
+        <div id="picture2"></div>
+        <div id="picture3"></div>
+    </div>
+    <div id="allPicturesTextBox">
+        <p class="allTipsAllPictures">se hela bildgalleriet</p>
+    </div>
+    <p id="quote">"Det börjar alltid med en viskning"</p>
+  `;
   main.style.display = "flex";
   main.style.flexDirection = "column";
   main.style.alignItems = "center";
-
-  let whisperBox = document.createElement("div");
-  whisperBox.id = "whisperBox";
-  let whisperTitle = document.createElement("h1");
-  whisperTitle.textContent = "Senaste viskningar...";
-  whisperTitle.id = "whisperTitle";
-  whisperBox.append(whisperTitle);
-
-  let div = document.createElement("div");
-  div.id = "gossipBox";
-
-  let allTipsBox = document.createElement("div");
-  allTipsBox.id = "allTipsBox";
-  let allTips = document.createElement("p");
-  allTips.textContent = "alla gossips";
-  allTips.id = "allTips";
-  allTipsBox.append(allTips);
-
-  main.append(whisperBox);
-  main.append(div);
-
-  main.append(allTipsBox);
 }
 
 homePage();
