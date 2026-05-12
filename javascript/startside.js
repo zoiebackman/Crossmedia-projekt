3c6a1f84fd8681a289ced06a75e4c6153202138d
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2026-05-12 13:43:44 +0200

    .

--- javascript/startside.js
+++ javascript/startside.js
@@ -133,69 +133,42 @@ function homePage() {
   });
 }
 
+welcomeNav.addEventListener("click", welcomeFun)
+
 function welcomeFun() {
-  footer.style.display = "block";
+
   main.innerHTML = ``;
+  main.innerHTML = `
+   <div id="welcomeContainer">
+   <div id="welcomeWrapper">
+        <h1 id="WelcomeText">Välkommen till spelet</h1>
+        <p id="introText">Terminen har precis börjat på Malmö universitet, men istället för att fokusera på fastighetsrätt och kundmöten har du vaknat upp till din värsta mardröm. Baksmällan från gårdagens fest på Niagaras takvåning är ingenting jämfört med den ångest som sköljer över dig när du ser ditt namn pryda rubrikerna på MAU Gossip. Du är Alice. Du brukar hålla dig i bakgrunden, men nu är du indragen i ett nät av lögner, hemligheter och maktspel som styrs av Malmös absoluta elit. Ditt rykte och din framtida karriär står på spel.</p>
 
-  const welcomeContainer = document.createElement("div");
-  welcomeContainer.id = "container";
+        <div id="welcomeSection1">
+            <h2 id="howToPlay">Så här spelar du:</h2>
+            <p id="welcomeInstructions">I det här spelet är din mobil och din förmåga att lägga pussel dina viktigaste verktyg. För att rentvå ditt namn och hitta personen bakom skandalbloggen måste du navigera genom följande:</p>
 
-  const popUpContainer = document.createElement("div");
-  popUpContainer.id = "popUpContainer";
+            <ul>
+                <li><strong>Granska sociala medier:</strong> Håll koll på Instagram-stories, inlägg och kommentarer. Varje bild kan innehålla en detalj som andra missat.</li>
+                <li><strong>Hitta ledtrådar i verkligheten: </strong> Besök fysiska platser på campus, såsom biblioteket, kaféet och Niagaras tak. Letar du noga kan du hitta allt från kvarglömda böcker till gömda lösenord.</li>
+                <li><strong>:Kartläggning </strong> Genom att hitta användarnamn och lösenord får du tillgång till privata chattar och dolda sidor. Men var försiktig – det du hittar kan förändra din bild av dina klasskamrater för alltid.</li>
+                <li>strong>Dra slutsatser: </strong> Koppla ihop koordinater, trasiga fotografier och anonyma tips för att lista ut vem som egentligen pratar med vem – och varför.</li>
+            </ul>
+        </div>
+          <h3 id="universityTitle">Fastighetsförmedling ME2024</h3>
+        <div id="imgContainer"></div>
+        <button id="startButton">Start</button>
+        </div>
+    </div>
+  `;
 
-  const headerText = document.createElement("h1");
-  headerText.id = "headerText";
-  headerText.textContent = "Välkomna till spelet";
-  popUpContainer.append(headerText);
-  main.append(welcomeContainer);
-  welcomeContainer.append(popUpContainer);
+  const startButton = document.getElementById("startButton");
 
-  const textBox = document.createElement("div");
-  textBox.id = "textBox";
-  popUpContainer.append(textBox);
-
-  const storyText = document.createElement("p");
-  storyText.id = "storyText";
-  storyText.textContent =
-    "Terminen har precis börjat på Malmö universitet, men istället för att fokusera på fastighetsrätt och kundmöten har du vaknat upp till din värsta mardröm. Baksmällan från gårdagens fest på Niagaras takvåning är ingenting jämfört med den ångest som sköljer över dig när du ser ditt namn pryda rubrikerna på MAU Gossip. Du är Alice. Du brukar hålla dig i bakgrunden, men nu är du indragen i ett nät av lögner, hemligheter och maktspel som styrs av Malmös absoluta elit. Ditt rykte och din framtida karriär står på spel.";
-  textBox.append(storyText);
-
-  const textBox2 = document.createElement("div");
-  textBox2.id = "textBox2";
-  const howToPlay = document.createElement("div");
-  howToPlay.innerHTML = `
-      <div id="playRules">
-        <h3>Så här spelar du: </h3>
-      </div>
-        <p>I det här spelet är din mobil och din förmåga att lägga pussel dina viktigaste verktyg. För att rentvå ditt namn och hitta personen bakom skandalbloggen måste du navigera genom följande: </p>
-        <ul>
-          <li><strong>Granska sociala medier:</strong> Håll koll på Instagram-stories, inlägg och kommentarer. Varje bild kan innehålla en detalj som andra missat.</li>
-          <li><strong>Hitta ledtrådar i verkligheten: </strong> Besök fysiska platser på campus, såsom biblioteket, kaféet och Niagaras tak. Letar du noga kan du hitta allt från kvarglömda böcker till gömda lösenord.</li>
-          <li><strong>Infiltration: </strong> Genom att hitta användarnamn och lösenord får du tillgång till privata chattar och dolda sidor. Men var försiktig – det du hittar kan förändra din bild av dina klasskamrater för alltid.</li>
-          <li><strong>Dra slutsatser: </strong> Koppla ihop koordinater, trasiga fotografier och anonyma tips för att lista ut vem som egentligen pratar med vem – och varför.</li>
-        </ul>`;
-
-  textBox.append(howToPlay);
-  textBox.append(textBox2);
-
-  const univeristyTitle = document.createElement("h2");
-  univeristyTitle.id = "universityTitle";
-  univeristyTitle.textContent = "Fastighetsförmedling ME2024";
-  popUpContainer.append(univeristyTitle);
-
-  const imgContainer = document.createElement("img");
-  imgContainer.id = "imgContainer";
-  popUpContainer.append(imgContainer);
-
-  const startButton = document.createElement("button");
-  startButton.id = "startButton";
-  startButton.textContent = "Start";
-  popUpContainer.append(startButton);
-
-  startButton.addEventListener("click", function () {
-    homePage();
-  });
+  startButton.addEventListener("click", function (){
+    homePage()
+  })
 }
+
 //rensa main och visa Alla viskningar
 function goToWhisperPage() {
   let totalWhisperPosts;
