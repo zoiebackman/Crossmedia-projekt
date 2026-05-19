8f93f642bc4845edc98e2c58390d649a86102ca8
zoiebackman <zoiebackman@icloud.com>
2026-05-19 09:51:14 +0200

    .

--- funktioner/functions.js
+++
@@ -1,7 +0,0 @@
-export async function getJsonInformation(jsonFile) {
-  const file = jsonFile;
-  const json = await Deno.readTextFile(file);
-  const jsonArray = JSON.parse(json);
-
-  return jsonArray;
-}
